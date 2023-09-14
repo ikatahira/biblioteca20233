c819fbe2505ef357e5161f58d76ca36d9cefba5b
ikatahira <isaque.katahira@fatec.sp.gov.br>
2023-09-14 19:30:32 +0000

    GeneroController

--- app/src/main/java/application/model/GeneroRepository.java
+++ app/src/main/java/application/model/GeneroRepository.java
@@ -2,6 +2,6 @@ package application.model;
 
 import org.springframework.data.repository.CrudRepository;
 
-public class GeneroRepository extends CrudRepository<Genero, Integer> {
+public interface GeneroRepository extends CrudRepository<Genero, Integer> {
     
 }
